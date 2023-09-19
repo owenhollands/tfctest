050a76da22ba5c1652a16337fca43ec3cf5e091a
Owen Hollands <owenhollands@gmail.com>
2023-09-19 15:08:51 +1000

    Fix name case

--- filesystems/git-code/Iiot-hub.tf
+++ filesystems/git-code/Iiot-hub.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "this" {
-  name                     = "storAccHub1"
+  name                     = "hub1-stor-ac"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -12,20 +12,20 @@ resource "azurerm_storage_account" "this" {
 }
 
 resource "azurerm_storage_container" "this" {
-  name                  = "storConHub1"
+  name                  = "hub1-stor-con"
   storage_account_name  = azurerm_storage_account.this.name
   container_access_type = "private"
 }
 
 resource "azurerm_eventhub_namespace" "this" {
-  name                = "nSpaceHib1"
+  name                = "hub1-eh-namespace"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
 }
 
 resource "azurerm_eventhub" "this" {
-  name                = "eHubHub1"
+  name                = "hub1-eh"
   resource_group_name = azurerm_resource_group.rg.name
   namespace_name      = azurerm_eventhub_namespace.this.name
   partition_count     = 2
@@ -36,12 +36,12 @@ resource "azurerm_eventhub_authorization_rule" "this" {
   resource_group_name = azurerm_resource_group.rg.name
   namespace_name      = azurerm_eventhub_namespace.this.name
   eventhub_name       = azurerm_eventhub.this.name
-  name                = "eHubRuleHub1"
+  name                = "hub1-eh-ar"
   send                = true
 }
 
 resource "azurerm_iothub" "this" {
-  name                         = "iotHubHub1"
+  name                         = "hub1-iothub"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   local_authentication_enabled = false
@@ -54,7 +54,7 @@ resource "azurerm_iothub" "this" {
   endpoint {
     type                       = "AzureIotHub.StorageContainer"
     connection_string          = azurerm_storage_account.this.primary_blob_connection_string
-    name                       = "export"
+    name                       = "hub1-ep-stor-con"
     batch_frequency_in_seconds = 60
     max_chunk_size_in_bytes    = 10485760
     container_name             = azurerm_storage_container.this.name
@@ -65,24 +65,10 @@ resource "azurerm_iothub" "this" {
   endpoint {
     type              = "AzureIotHub.EventHub"
     connection_string = azurerm_eventhub_authorization_rule.this.primary_connection_string
-    name              = "export2"
+    name              = "hub1-ep-eh"
   }
 
-  route {
-    name           = "export"
-    source         = "DeviceMessages"
-    condition      = "true"
-    endpoint_names = ["export"]
-    enabled        = true
-  }
 
-  route {
-    name           = "export2"
-    source         = "DeviceMessages"
-    condition      = "true"
-    endpoint_names = ["export2"]
-    enabled        = true
-  }
 
   enrichment {
     key            = "tenant"
