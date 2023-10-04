0ff8c1830d3c51e4f3b76ce9b8f8a2a8c62e3181
Owen Hollands <owenhollands@gmail.com>
2023-10-04 12:28:52 +1100

    Add an iot central instance

---
+++ filesystems/git-code/iot-central.tf
@@ -0,0 +1,19 @@
+resource "azurerm_resource_group" "example" {
+  name     = "iot-hub-central-01"
+  location = "West Europe"
+}
+
+resource "azurerm_iotcentral_application" "example" {
+  name                = "iotcentral-app-01"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sub_domain          = "iotcentral-app-subdomain-01"
+
+  display_name = "iotcentral-app-display-name-01"
+  sku          = "ST1"
+  template     = "iotc-default@1.0.0"
+
+  tags = {
+    Foo = "Bar"
+  }
+}
